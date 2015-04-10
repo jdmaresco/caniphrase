f7cca9aa017f4a196fcf41d1e03cbbaea0ce0cd8
JD Maresco <jdmaresco@gmail.com>
2015-04-09 22:48:00 -0400

    Add firebase auth

--- js/main.js
+++ js/main.js
@@ -1,1 +1,43 @@
-var temp = function() {}
+//var ref = new Firebase("https://caniphrase.firebaseio.com");
+var ref = new Firebase("https://caniphrase.firebaseio.com/phrases")
+
+var authenticateWithGoogle = function() {
+  ref.authWithOAuthPopup("google", function(error, authData) {
+    if (error) {
+      console.log("Login Failed!", error);
+    } else {
+      console.log("Authenticated successfully with payload:", authData);
+    }
+  });
+}
+
+// Create a callback which logs the current auth state
+function authDataCallback(authData) {
+  if (authData) {
+    console.log("User " + authData.uid + " is logged in with " + authData.provider);
+  } else {
+    console.log("User is logged out");
+  }
+}
+
+
+$(function(){
+  $('#loginButton').on('click', function(e) {
+    e.preventDefault();
+    authenticateWithGoogle();
+  })
+
+  $('#logoutButton').on('click', function(e) {
+    e.preventDefault();
+    ref.unauth();
+  })
+
+  ref.on("child_added", function(snapshot) {
+    console.log(snapshot.val());
+  }, function (errorObject) {
+    console.log("The read failed: " + errorObject.code);
+  });
+
+  ref.onAuth(authDataCallback);
+
+})
